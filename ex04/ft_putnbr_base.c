d74be5e64201354997d2fbaa284f398099e26891
Benjy Malca Bautista <bmalca-b@car10s5.42barcelona.com>
2025-09-01 20:13:55 +0200

    WIP : putnbr

---
+++ ex04/ft_putnbr_base.c
@@ -0,0 +1,63 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_putnbr_base.c                                   :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: bmalca-b <bmalca-b@student.42barcelon      +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2025/09/01 18:37:07 by bmalca-b          #+#    #+#             */
+/*   Updated: 2025/09/01 20:07:30 by bmalca-b         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+
+void	print_number(int nb, char *base_str, int base)
+{
+	char	out;
+
+	if (nb >= base)
+		print_number(nb / base, base_str, base);
+	out = base_str[(nb % base)];
+	write(1, &out, 1);
+}
+/*
+void	ft_putnbr(int nb)
+{
+	if (nb < 0)
+	{
+		write(1, "-", 1);
+		nb = nb * -1;
+	}
+	print_number(nb);
+}
+*/
+int	validate_base(char *dirty_base)
+{
+	int size;
+
+	size = 0;
+	while(dirty_base[size] != '\0')
+	{
+		size++;
+	}
+	return (size);
+}
+
+void	ft_putnbr_base(int nbr, char *base)
+{
+	int base_size;
+
+	base_size = validate_base(base);
+	print_number(nbr, base, base_size);
+}
+
+#include <stdio.h>
+#include <stdlib.h>
+int	main(int argc, char **argv)
+{
+	
+	ft_putnbr_base(atoi(argv[1]), argv[2]);
+	
+	return (argc * 0);
+}
